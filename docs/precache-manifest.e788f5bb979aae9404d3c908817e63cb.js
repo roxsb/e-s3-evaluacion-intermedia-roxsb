64441856d017b3d05f4c0136b4efcefbcc881b3d
roxsb <rox.san.bri@gmail.com>
2019-01-15 11:37:56 +0100

    add docs pages

---
+++ docs/precache-manifest.e788f5bb979aae9404d3c908817e63cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7665a0d384ff99e763d9",
+    "url": "./static/js/main.7665a0d3.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "27750d7b3474583cc337b02d85ba183c",
+    "url": "./index.html"
+  }
+];
